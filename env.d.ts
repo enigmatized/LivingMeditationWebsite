b0d057504e5634a495736bb3ebe0fc405c94e8ab
enigmatized <going.to.any.lengths.work@gmail.com>
2025-01-18 16:25:36 -0600

    Is chatgpt god?

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+// src/env.d.ts or src/shims-vue.d.ts
+declare module '*.vue' {
+    import { DefineComponent } from 'vue'
+    const component: DefineComponent<{}, {}, any>
+    export default component
+  }
+  
